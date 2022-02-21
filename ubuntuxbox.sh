1de8393e418234ec16f3e870a672a93eef229990
ScriptsAndApps <99158605+ScriptsAndApps@users.noreply.github.com>
2022-02-21 03:10:55 -0500

    Update ubuntuxbox.sh

--- ubuntuxbox.sh
+++ ubuntuxbox.sh
@@ -1,10 +1,16 @@
+sudo cp /etc/apt/sources.list sources.list.bak
+#on error try software-properties-gtk
 sudo echo -e "\n#this are the new lines\ndeb cdrom:[Ubuntu 21.10 _Impish Indri_ - Release amd64 (20211012)]/ impish main restricted\ndeb http://archive.ubuntu.com/ubuntu/ impish main restricted\ndeb http://security.ubuntu.com/ubuntu/ impish-security main restricted\ndeb http://archive.ubuntu.com/ubuntu/ impish-updates main restricted\ndeb http://us.archive.ubuntu.com/ubuntu/ hirsute main restricted\ndeb-src http://us.archive.ubuntu.com/ubuntu/ hirsute main restricted\ndeb http://us.archive.ubuntu.com/ubuntu/ hirsute-updates main restricted\ndeb-src http://us.archive.ubuntu.com/ubuntu/ hirsute-updates main restricted\ndeb http://us.archive.ubuntu.com/ubuntu/ hirsute universe\ndeb-src http://us.archive.ubuntu.com/ubuntu/ hirsute universe\ndeb http://us.archive.ubuntu.com/ubuntu/ hirsute-updates universe\ndeb-src http://us.archive.ubuntu.com/ubuntu/ hirsute-updates universe\ndeb http://us.archive.ubuntu.com/ubuntu/ hirsute multiverse\ndeb-src http://us.archive.ubuntu.com/ubuntu/ hirsute multiverse\ndeb http://us.archive.ubuntu.com/ubuntu/ hirsute-updates multiverse\ndeb-src http://us.archive.ubuntu.com/ubuntu/ hirsute-updates multiverse\ndeb http://us.archive.ubuntu.com/ubuntu/ hirsute-backports main restricted universe multiverse\ndeb-src http://us.archive.ubuntu.com/ubuntu/ hirsute-backports main restricted universe multiverse\ndeb http://archive.canonical.com/ubuntu hirsute partner\ndeb-src http://archive.canonical.com/ubuntu hirsute partner\ndeb http://security.ubuntu.com/ubuntu hirsute-security main restricted\ndeb-src http://security.ubuntu.com/ubuntu hirsute-security main restricted\ndeb http://security.ubuntu.com/ubuntu hirsute-security universe\ndeb-src http://security.ubuntu.com/ubuntu hirsute-security universe\ndeb http://security.ubuntu.com/ubuntu hirsute-security multiverse\ndeb-src http://security.ubuntu.com/ubuntu hirsute-security multiverse" >> /etc/apt/sources.list
-
+sudo sort /etc/apt/sources.list | uniq > temp.list
+sudo cp temp.list /etc/apt/sources.list
+sudo rm temp.list
+sudo cat /etc/apt/sources.list
 sudo apt-add-repository -y ppa:rael-gc/ubuntu-xboxdrv
 sudo apt-get update
 sudo apt-get install ubuntu-xboxdrv
 sudo systemctl enable xboxdrv.service
 sudo systemctl start xboxdrv.service
+sudo modprobe xpad
 
 wget http://ppa.launchpad.net/mdeslaur/steamos/ubuntu/pool/main/s/steamos-xpad-dkms/steamos-xpad-dkms_0.5-0md0.16.04.1_all.deb
 sudo dpkg -i steamos-xpad-dkms_0.5-0md0.16.04.1_all.deb
@@ -13,12 +19,6 @@ sudo apt-get install -f
 sudo apt install git
 git clone https://github.com/paroj/xpad.git /usr/src/xpad-0.4
 dkms install -m xpad -v 0.4
-sudo modprobe xpad
-sudo lsusb
-echo try sudo lsusb
-echo on no connect
-echo try: sudo modprobe xpad
-sudo systemctl restart xboxdrv.service
 sudo modprobe xpad
 
 #sudo apt-get install xboxdrv
@@ -37,3 +37,8 @@ sudo apt install mono-complete
 sudo apt install winetricks
 sudo winetricks dotnet45
 
+sudo lsusb
+echo try sudo lsusb
+echo on no connect
+echo try: sudo modprobe xpad
+sudo modprobe xpad
