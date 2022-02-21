31d40b80997a5d7f2990548f9e3b6a0fc829fb8a
ScriptsAndApps <99158605+ScriptsAndApps@users.noreply.github.com>
2022-02-21 01:51:32 +0000

    Update ubuntuxbox.sh

--- ubuntuxbox.sh
+++ ubuntuxbox.sh
@@ -31,6 +31,5 @@ sudo modprobe xpad
 sudo apt install wine
 
 echo https://www.x360ce.com/
-sudo dpkg --add-architecture i386 && apt-get update &&
-apt-get install wine32
+sudo dpkg --add-architecture i386 && apt-get update && apt-get install wine32
 
